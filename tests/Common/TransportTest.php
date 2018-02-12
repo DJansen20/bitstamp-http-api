c7a92365240bf01b44b3e20b1baec9e2d567e04d
Danny Jansen <danny.jansen93@gmail.com>
2018-02-12 23:59:05 +0100

    DEV-50 | Add tests for Transport class

--- tests/Common/TransportTest.php
+++ tests/Common/TransportTest.php
@@ -7,6 +7,8 @@
 
 namespace Bitstamp\Tests\Common;
 
+use Bitstamp\Models\CurrencyPair;
+use Bitstamp\PublicApi\Requests\TickerRequest;
 use PHPUnit\Framework\TestCase;
 
 use Bitstamp\Common\Transport;
@@ -24,6 +26,25 @@ class TransportTest extends TestCase
     {
         $transport = new Transport();
         $this->assertInstanceOf('Bitstamp\\Common\\Transport', $transport);
+        return $transport;
+    }
+
+    /**
+     * @depends testCanBeConstructed
+     * @covers \Bitstamp\Common\Transport::send
+     * @covers \Bitstamp\Common\Transport::buildUrl
+     *
+     * @param Transport $transport
+     * @return Transport
+     * @throws \Bitstamp\Exception\BitstampEndpointException
+     * @throws \Exception
+     */
+    public function testCanSendRequest(Transport $transport): Transport
+    {
+        $request = new TickerRequest(CurrencyPair::BTCUSD);
+        $response = $transport->send($request);
+        $this->assertNotEmpty($response);
+        $this->assertJson($response);
         return $transport;
     }
 }

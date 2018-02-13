76e2774f8699791e406062410e1d4c3f1bf53145
Danny Jansen <d.jansen@rootnet.nl>
2018-02-13 08:58:58 +0100

    DEV-50 | Add TransactionRequest tests

---
+++ tests/PublicApi/Requests/TransactionsRequestTest.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * @package Bitstamp HTTP API
+ * @author Danny Jansen <danny.jansen93@gmail.com>
+ * @license: MIT
+ */
+
+namespace Bitstamp\Tests\PublicApi\Requests;
+
+use PHPUnit\Framework\TestCase;
+use Bitstamp\Models\CurrencyPair;
+use Bitstamp\PublicApi\Requests\TransactionsRequest;
+
+class TransactionsRequestTest extends TestCase
+{
+    /**
+     * Check if request can be constructed
+     *
+     * @covers \Bitstamp\PublicApi\Requests\TransactionsRequest::__construct
+     *
+     * @return TransactionsRequest
+     * @throws \Bitstamp\Exception\BitstampEndpointException
+     * @throws \Exception
+     */
+    public function testCanBeConstructed(): TransactionsRequest
+    {
+        $request = new TransactionsRequest(CurrencyPair::BCHBTC);
+        $this->assertInstanceOf('Bitstamp\\PublicApi\\Requests\\TransactionsRequest', $request);
+
+        return $request;
+    }
+
+    /**
+     * @depends testCanBeConstructed
+     * @covers \Bitstamp\PublicApi\Requests\TransactionsRequest::getCurrencyPair
+     *
+     * @param TransactionsRequest $request
+     * @throws \Exception
+     */
+    public function testGetCurrencyPair(TransactionsRequest $request): void
+    {
+        $this->assertEquals('bchbtc', $request->getCurrencyPair());
+    }
+
+    /**
+     * @depends testCanBeConstructed
+     * @covers \Bitstamp\PublicApi\Requests\TransactionsRequest::setCurrencyPair
+     *
+     * @param TransactionsRequest $request
+     * @throws \Exception
+     */
+    public function testSetCurrencyPair(TransactionsRequest $request): void
+    {
+        $request->setCurrencyPair(CurrencyPair::ETHBTC);
+        $this->assertEquals(CurrencyPair::ETHBTC, $request->getCurrencyPair());
+    }
+
+    /**
+     * @depends testCanBeConstructed
+     * @covers \Bitstamp\PublicApi\Requests\TransactionsRequest::getTime
+     *
+     * @param TransactionsRequest $request
+     * @throws \Exception
+     */
+    public function testGetTime(TransactionsRequest $request): void
+    {
+        $this->assertEquals('hour', $request->getTime());
+    }
+
+    /**
+     * @depends testCanBeConstructed
+     * @covers \Bitstamp\PublicApi\Requests\TransactionsRequest::setTime
+     *
+     * @param TransactionsRequest $request
+     * @throws \Exception
+     */
+    public function testSetTime(TransactionsRequest $request): void
+    {
+        $request->setTime('minute');
+        $this->assertEquals('minute', $request->getTime());
+    }
+
+    /**
+     * @depends testCanBeConstructed
+     * @covers \Bitstamp\PublicApi\Requests\TransactionsRequest::withUri
+     *
+     * @param TransactionsRequest $request
+     * @return TransactionsRequest
+     * @throws \Exception
+     */
+    public function testWithUri(TransactionsRequest $request): TransactionsRequest
+    {
+        $request->setCurrencyPair(CurrencyPair::BCHBTC);
+        $request->setTime('hour');
+        $this->assertEquals('bchbtc', $request->getCurrencyPair());
+        $this->assertEquals('hour', $request->getTime());
+        $this->assertEquals('transactions/bchbtc?time=hour', $request->withUri());
+
+        return $request;
+    }
+}
